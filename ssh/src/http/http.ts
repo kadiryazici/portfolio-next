8491a7cc60fd5ccfc006e989f14caf354066cb86
Kadir Yazıcı <47540799+kadiryazici@users.noreply.github.com>
2026-08-28 03:34:32 +0300

    fix: change hostname

--- ssh/src/http/http.ts
+++ ssh/src/http/http.ts
@@ -2,7 +2,7 @@ import indexHtml from "./index.html"
 
 export function serveHttp() {
   Bun.serve({
-    hostname: "0.0.0.0",
+    hostname: "127.0.0.1",
     port: process.env.HTTP_PORT ?? 8080,
     routes: {
       "/*": indexHtml
